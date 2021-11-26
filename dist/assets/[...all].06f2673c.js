bad136e0f5afda3d609640d38de281c1854d61c0
Sapana Baniya <sapnabaniya.xdezo@gmail.com>
2021-11-26 14:53:01 +0545

    changes

---
+++ dist/assets/[...all].06f2673c.js
@@ -0,0 +1,1 @@
+import{g as t,h as e,o as a,b as s,t as r,j as c}from"./vendor.00a65a29.js";var n={};const f=t({setup(p){const{t:o}=e();return(u,i)=>(a(),s("div",null,r(c(o)("not-found")),1))}});typeof n=="function"&&n(f);export{f as default};
